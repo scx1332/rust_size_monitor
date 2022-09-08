dbfa564e055ac5fe73da3002b5516bed1435a1e8
scx1332 <sieciech@gmail.com>
2022-09-08 19:48:55 +0200

    Working on API

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,11 @@ use structopt::StructOpt;
 use ya_http_proxy_client::api::ManagementApi;
 use ya_http_proxy_client::web::{WebClient, DEFAULT_MANAGEMENT_API_URL};
 use ya_http_proxy_client::Error;
-use ya_http_proxy_model::{CreateService, GlobalStats, Service, User};
+use ya_http_proxy_model::{Addresses, CreateService, GlobalStats, Service, User};
 use tokio;
 use std::str::FromStr;
+use anyhow::anyhow;
+use hyper::body::HttpBody;
 
 #[derive(Debug)]
 struct PathInfo {
@@ -90,6 +92,107 @@ fn log_format(
     )
 }
 
+async fn get_erigon_service() -> anyhow::Result<Service>
+{
+    let api_url = "http://127.0.0.1:7777".to_string();
+    let client = WebClient::new(api_url.to_string()).map_err(anyhow::Error::from)?;
+    let api = ManagementApi::new(client);
+
+    api.get_service("erigon").await.map_err(anyhow::Error::from)
+}
+
+async fn create_erigon_endpoint() -> anyhow::Result<()>
+{
+    let api_url = "http://127.0.0.1:7777".to_string();
+    let client = WebClient::new(api_url.to_string()).map_err(anyhow::Error::from)?;
+    let api = ManagementApi::new(client);
+
+    let addresses = Addresses::new(vec!(SocketAddr::from_str("0.0.0.0:11120").unwrap()));
+    let from_uri = Uri::from_str("/").unwrap();
+    let to_uri = Uri::from_str("http://127.0.0.1/").unwrap();
+    let cs = CreateService{
+        name: "erigon".to_string(),
+        server_name: vec!["0.0.0.0".to_string()],
+        bind_https: None,
+        bind_http: Some(addresses),
+        cert: None,
+        auth: None,
+        from: from_uri,
+        to: to_uri,
+        timeouts: None,
+        cpu_threads: None,
+        user: None
+    };
+    api.create_service(&cs).await?;
+    Ok(())
+}
+
+async fn create_erigon_endp() -> anyhow::Result<()>
+{
+    let mut service = match get_erigon_service().await {
+        Ok(service) => Some(service),
+        Err(err) => {
+            //todo: check if really error or just not exists
+            None
+        }
+    };
+    let service = match service {
+        Some(service) => service,
+        None => {
+            create_erigon_endpoint().await?;
+            //Ok(()) => "Created successfully".to_string(),
+            //Err(err) => return Err(anyhow!(format!("Error when adding service {err}!")))
+            //}
+            match get_erigon_service().await {
+                Ok(service) => service,
+                Err(err) => return Err(anyhow!("Unknown error when creating service"))
+            }
+        }
+    };
+
+
+
+    Ok(())
+}
+
+async fn list_services_help() -> anyhow::Result<Vec<Service>>
+{
+    let api_url = "http://127.0.0.1:7777".to_string();
+    let client = WebClient::new(api_url.to_string()).map_err(anyhow::Error::from)?;
+    let api = ManagementApi::new(client);
+
+    api.get_services().await.map_err(anyhow::Error::from)
+}
+
+#[get("/list_services")]
+async fn list_services() -> HttpResponse {
+    match list_services_help().await {
+
+        Ok(services) => {
+            let body = serde_json::to_string(&services).unwrap();
+            HttpResponse::Ok()
+                .content_type("application/json")
+                .body(body)
+        },
+        Err(err) => HttpResponse::NotFound().content_type("plain/text").body(format!("Error when adding service {err}!"))
+    }
+}
+
+#[get("/create_erigon")]
+async fn create_erigon() -> HttpResponse {
+    match create_erigon_endp().await {
+        Ok(services) => {
+            let body = "{\"result\":\"success\"}";
+            HttpResponse::Ok()
+                .content_type("application/json")
+                .body(body)
+        },
+        Err(err) => HttpResponse::NotFound().content_type("plain/text").body(format!("Error when creating service {err}!"))
+    }
+}
+
+
+
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {name}!")
@@ -136,30 +239,37 @@ async fn main() -> anyhow::Result<()> {
         println!("Found person {:?}", person.unwrap());
     }
 
+
+    /*
     let api_url = cli.management_addr.clone();
     let client = WebClient::new(api_url.to_string())?;
     let api = ManagementApi::new(client);
-    
+
+    let addresses = Addresses::new(vec!(SocketAddr::from_str("0.0.0.0:11120").unwrap()));
+    let from_uri = Uri::from_str("/").unwrap();
+    let to_uri = Uri::from_str("http://127.0.0.1/").unwrap();
     let cs = CreateService{
         name: "Erigon".to_string(),
-        server_name: vec![],
+        server_name: vec!["0.0.0.0".to_string()],
         bind_https: None,
-        bind_http: None,
+        bind_http: Some(addresses),
         cert: None,
         auth: None,
-        from: Uri::default(),
-        to: Uri::default(),
+        from: from_uri,
+        to: to_uri,
         timeouts: None,
         cpu_threads: None,
         user: None
     }; 
     api.create_service(&cs).await?;
-
+*/
 
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(HttpResponse::Ok))
             .service(greet)
+            .service(create_erigon)
+            .service(list_services)
     })
     .bind(("127.0.0.1", 8080))
     .map_err(anyhow::Error::from)?
